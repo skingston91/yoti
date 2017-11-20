8cbccdeedb14b004af1186fbfcf3b5b677cb2bd8
Steven Kingston <skingston.dev@gmail.com>
2017-11-20 22:54:46 +0000

    Tidied up the code base, removed redux, updated readme

--- README.md
+++ README.md
@@ -5,5 +5,15 @@
 	> yarn install
 	> yarn start
 	> localhost:8080 for the website
-
 ```
+
+Things I would have liked to have added:
+	- Locally scoped variables seperate to the stylesheets classes
+	- Fixed styling to be closer
+	- Moved to ternary for render checks
+	- Snapshot tests for component renders
+	- Unit tests for sortGroup method
+	- Stripped out moments locales
+	- Finished the popupModal
+	- Added hover effects
+	- Dug deeper into package.json for unused modules
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import reducers from './reducers';
 import App from './containers/App';
-import thunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
-)(createStore);
 
 ReactDOM.render(
-  <Provider store={ createStoreWithMiddleware(reducers) }>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <Router>
+    <App />
+  </Router>,
   document.querySelector('.rootContainer')
 );
